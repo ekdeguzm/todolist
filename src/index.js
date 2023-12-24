2d948b39e0e55f9198276e3d86c978768ef88f26
Kawayan <kawayan.deguzman@gmail.com>
2023-12-24 14:12:12 -0800

    test webpack

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import _ from 'lodash';
 // testing out webpack initial setup of index.js
 console.log("testing index.js webpack")
 
-
+// testing out DOM manipulation via webpack
+const contentDiv = document.querySelector(".content");
+const testH1 = document.createElement("h1");
+testH1.textContent = "HELLO WORLD... TEST FROM WEBPACK INDEX.JS DOM MANIPULATON"
+contentDiv.appendChild(testH1);
 
 function component() {
   const element = document.createElement('div');
